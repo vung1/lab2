556948722a1b7156a5a89316a8c11fe226ee1405
Victor Ung <vung@ucsd.edu>
2021-01-13 12:27:29 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,9 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
     initializePage();
 });
 
 function initializePage() {
-    $('#testjs').click(function(e) {
-            $('.jumbotron h1').text("Javascript is connected");
-            });
+    $("#testjs").click(function (e) {
+        $(".jumbotron h1").text("Javascript is connected");
+    });
+
+    $("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+    // prevent the page from reloading
+    e.preventDefault();
+
+    $(this).css("background-color", "#7fff00");
 }
